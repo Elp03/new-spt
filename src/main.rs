34d8d0d6ab041bc9c122b5670c3c1e934e34b65e
Athamantis <helle@grasmo.no>
2024-08-28 20:19:31 +0200

    Fix own spotify comand file. Make function for searching and play songs, albums and artist

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #![allow(clippy::assigning_clones)]
 
-use std::any::Any;
+use std::{any::Any,
+    io::{stdout, Result},
+    time::Duration,
+    fs::File};
+
+use log::{info, warn, error}; 
+
 
 use rspotify::{model::{album, artist, playlist, user, AlbumId, Market, SearchResult, SearchType, SimplifiedPlaylist, TrackId, Type, UserId}, prelude::*, scopes, AuthCodePkceSpotify, Credentials, OAuth};
-use spotify::PlaySpotify;
+use spotify::{album_reslults, artist_results, tracs_results, PlaySpotify};
+use simplelog::*;
 
 
 mod spotify; 
@@ -17,7 +24,7 @@ async fn main() {
    // let history = spotify.current_playback(None, None::<Vec<_>>);
     //println!("Response after refreshing token: {history:?}");
 
-    let mut user_playlists: Vec<SimplifiedPlaylist> = Vec::new();
+    //let mut user_playlists: Vec<SimplifiedPlaylist> = Vec::new();
     //let user_playlist = *spotify.current_user_playlists();
     // for playlist in spotify.current_user_playlists() {
     //     match playlist {
@@ -25,82 +32,147 @@ async fn main() {
     //         Err(e) => println!("{:#?}", e)
     //     }
     // }
-    user_playlists = play_spotify.get_user_playlists(); 
-    println!("Here is user playlist {:#?}", user_playlists);
+    //user_playlists = play_spotify.get_user_playlists(); 
+    //println!("Here is user playlist {:#?}", user_playlists);
 
    // println!("This is current users playlists {:?}", user_playlists); 
    
-    let search_query = "This is palaye royal"; 
-    let mut search_result: Vec<SearchResult> = Vec::new();
-    let track_uri;
-    let seartch_spotify = spotify.search(&search_query, SearchType::Album, None, None, Some(3), None); 
-    let play_this_playlist: PlayContextId;  
+    let search_query = "guns n roses"; 
+   // let mut search_result: Vec<SearchResult> = Vec::new();
+    //let track_uri;
+    //let seartch_spotify = spotify.search(&search_query, SearchType::Album, None, None, Some(3), None); 
+    //let play_this_playlist: PlayContextId;  
 
    // #[derive(Debug)]
-    let playable_id: PlayableId; 
+    //let mut spotify_test: Vec<tracs_results> = Vec::new();
+    //spotify_test = play_spotify.search_tracks(&search_query, 3).expect("msg");
 
-    match seartch_spotify {
-        Ok(SearchResult::Artists(artist_page)) => {
-            if let Some(artist) = artist_page.items.get(0) {
-                println!("Artist found: {}", artist.name);
-                println!("Popularity: {}", artist.popularity);
-                println!("Followers: {}", artist.followers.total);
-                println!("Genres: {:?}", artist.genres);
-            } else {
-                println!("No artist found with the name '{}'", search_query);
-            }
-        },
-        Ok(SearchResult::Albums(albums)) => {
-            println!("Albums found: {}", albums.items.len());
-            println!("Album: {:#?}", albums);
-            let album_test: Vec<AlbumId> = Vec::new(); 
+    // play_spotify.search_tracks(search_query, 5);
+    // play_spotify.play_song(1);
 
-            // for playlist in albums.items{
-            //     let foo = playlist.artists;
-            //     let foo_bar = &playlist.id; 
-            //     //let bar = &foo[0].id;
+    // play_spotify.search_album(search_query, 3);
+    // play_spotify.play_album(1)
 
-            //     println!("here is the foobar {:#?}", foo_bar); 
-            // }
+    play_spotify.search_artist(search_query, 5);
+    play_spotify.play_artist(0);
+
+    // let spotify_test_test ={
+    //     match play_spotify.search_tracks(search_query, 3) {
+    //         Ok(results) => {
+    //             // Iterate through and print each track result
+    //             for rec in &results {
+    //                 println!("Track Name: {}", rec.name);
+    //                 println!("Artist: {}", rec.artist);
+    //             // println!("Playable ID: {:?}", rec.playable_id); // Use {:?} if `playable_id` implements Debug
+    //                 println!("------------------------------------");
+    //             }
+    //             results
+    //         }
+    //         Err(e) => {
+    //             // Handle and print the error
+    //             println!("Error occurred while searching tracks: {:?}", e);
+    //             Vec::new()
+    //         }
+    //     }
+    // };
+
+    // let mut artist_test: Vec<artist_results> = Vec::new();
+    // match play_spotify.search_artist(search_query, 3) {
+    //     Ok(results) => {
+    //         for rec in results {
+    //             println!("Artist Name: {}", rec.name);
+    //            // println!("Playable ID: {:?}", rec.playable_id); // Use {:?} if `playable_id` implements Debug
+    //             println!("------------------------------------");
+    //         }
+    //     }
+    //     Err(e)=> {
+    //         println!("Error occurred while searching tracks: {:?}", e);
+    //     }
+    // }
+
+    // let mut album_test: Vec<album_reslults> = Vec::new();
+    // match play_spotify.search_album(search_query, 3) {
+    //     Ok(results) => {
+    //         for rec in results {
+    //             println!("Album Name: {}", rec.album_name);
+    //             println!("Album maker: {}", rec.album_maker);
+    //            // println!("Playable ID: {:?}", rec.playable_id); // Use {:?} if `playable_id` implements Debug
+    //             println!("------------------------------------");
+    //         }
+    //     }
+    //     Err(e)=> {
+    //         println!("Error occurred while searching tracks: {:?}", e);
+    //     }
+    // }
+    
+    // let playsong = play_spotify.spotify.start_uris_playback(vec![test], None, None, None);
+    // match playsong {
+    //     Ok(data) => println!("{:#?}", data),
+    //     Err(e) => println!("error {:?}", e),
+    // }
+
+    // match seartch_spotify {
+    //     Ok(SearchResult::Artists(artist_page)) => {
+    //         if let Some(artist) = artist_page.items.get(0) {
+    //             println!("Artist found: {}", artist.name);
+    //             println!("Popularity: {}", artist.popularity);
+    //             println!("Followers: {}", artist.followers.total);
+    //             println!("Genres: {:?}", artist.genres);
+    //         } else {
+    //             println!("No artist found with the name '{}'", search_query);
+    //         }
+    //     },
+    //     Ok(SearchResult::Albums(albums)) => {
+    //         println!("Albums found: {}", albums.items.len());
+    //         println!("Album: {:#?}", albums);
+    //         let album_test: Vec<AlbumId> = Vec::new(); 
+
+    //         // for playlist in albums.items{
+    //         //     let foo = playlist.artists;
+    //         //     let foo_bar = &playlist.id; 
+    //         //     //let bar = &foo[0].id;
+
+    //         //     println!("here is the foobar {:#?}", foo_bar); 
+    //         // }
             
-           // let something = &albums.items[0].id;
-           play_this_playlist = PlayContextId::Album(albums.items[0].id.clone().expect("Album is none"));
+    //        // let something = &albums.items[0].id;
+    //        play_this_playlist = PlayContextId::Album(albums.items[0].id.clone().expect("Album is none"));
             
             
-           // play_this_playlist = PlayableId::Album(something.clone().expect("msg"));
+    //        // play_this_playlist = PlayableId::Album(something.clone().expect("msg"));
 
-            spotify.start_context_playback(play_this_playlist, None, None, None); 
+    //         spotify.start_context_playback(play_this_playlist, None, None, None); 
 
-        },
-        Ok(SearchResult::Tracks(tracks)) => {
-            if let Some(tracks) = tracks.items.first() {
-                track_uri = &tracks.id; 
-                playable_id = PlayableId::Track(track_uri.clone().expect("msg"));
-                //track_uri = TrackId::from_id(&tracks.id).unwrap().uri();
-                println!("Track found: {} - URI: {:?}", tracks.name, playable_id.uri());
+    //     },
+    //     Ok(SearchResult::Tracks(tracks)) => {
+    //         if let Some(tracks) = tracks.items.first() {
+    //             track_uri = &tracks.id; 
+    //             playable_id = PlayableId::Track(track_uri.clone().expect("msg"));
+    //             //track_uri = TrackId::from_id(&tracks.id).unwrap().uri();
+    //             println!("Track found: {} - URI: {:?}", tracks.name, playable_id.uri());
                 
-                let playsong = spotify.start_uris_playback(vec![playable_id], None, None, None);
-                match playsong {
-                    Ok(data) => println!("{:#?}", data),
-                    Err(e) => println!("error {:?}", e),
-                }
-                // Now you can use this URI to play the track or for other purposes
-            } else {
-                println!("No track found for");
-            }
+    //             let playsong = spotify.start_uris_playback(vec![playable_id], None, None, None);
+    //             match playsong {
+    //                 Ok(data) => println!("{:#?}", data),
+    //                 Err(e) => println!("error {:?}", e),
+    //             }
+    //             // Now you can use this URI to play the track or for other purposes
+    //         } else {
+    //             println!("No track found for");
+    //         }
         
-        },
-        Ok(SearchResult::Playlists(playlists)) => {
-            println!("Playlists found: {}", playlists.items.len());
-        },
-        Ok(SearchResult::Shows(shows)) => {
-            println!("Shows found: {}", shows.items.len());
-        },
-        Ok(SearchResult::Episodes(episodes)) => {
-            println!("Episodes found: {}", episodes.items.len());
-        },
-        Err(e) => println!("{:#?}", e)
-    }
+    //     },
+    //     Ok(SearchResult::Playlists(playlists)) => {
+    //         println!("Playlists found: {}", playlists.items.len());
+    //     },
+    //     Ok(SearchResult::Shows(shows)) => {
+    //         println!("Shows found: {}", shows.items.len());
+    //     },
+    //     Ok(SearchResult::Episodes(episodes)) => {
+    //         println!("Episodes found: {}", episodes.items.len());
+    //     },
+    //     Err(e) => println!("{:#?}", e)
+    // }
     
     // for playlist in spotify.search(&search_query, SearchType::Artist, None, None, Some(1), None) {
     //     match playlist {
--- src/spotify.rs
+++ src/spotify.rs
@@ -1,13 +1,39 @@
 
-use rspotify::{model::{album, artist, playlist, user, AlbumId, Market, SearchResult, SearchType, SimplifiedPlaylist, TrackId, Type, UserId}, prelude::*, scopes, AuthCodePkceSpotify, Credentials, OAuth};
+use std::fmt::Error;
+//use anyhow::Ok;
+use std::result::Result::Ok;
+
+//use anyhow::Error; 
+use rspotify::{model::{album, artist, playlist, track, user, AlbumId, ArtistId, FullArtist, Market, Page, SearchResult, SearchType, SimplifiedPlaylist, TrackId, Type, UserId}, prelude::*, scopes, AuthCodePkceSpotify, ClientError, Credentials, OAuth};
 
 
-pub struct PlaySpotify{
+pub struct PlaySpotify<'a>{
     pub is_something_playing: bool, 
     pub spotify: AuthCodePkceSpotify,
+    pub results_artist: Vec<artist_results<'a>>,
+    pub results_album: Vec<album_reslults<'a>>,
+    pub results_tracks: Vec<tracs_results<'a>>,
+    
 }
 
-impl PlaySpotify {
+//#[derive(Debug)]
+pub struct tracs_results<'a>{
+    pub playable_id: PlayableId<'a>,
+    pub name: String, 
+    pub artist: String,
+}
+pub struct artist_results<'a>{
+    pub name: String, 
+    pub artist_id: ArtistId<'a>
+    
+}
+pub struct album_reslults<'a>{
+    pub album_name: String, 
+    pub album_maker: String, 
+    pub album_id: AlbumId<'a>
+}
+
+impl PlaySpotify <'_> {
 
     pub fn new() -> Self{
         // Using env logger for extra debugging. 
@@ -40,6 +66,9 @@ impl PlaySpotify {
         Self{
             is_something_playing: true,
             spotify: spotify,
+            results_album: Vec::new(),
+            results_artist: Vec::new(),
+            results_tracks: Vec::new(),
         }
     }
     
@@ -57,6 +86,162 @@ impl PlaySpotify {
         user_playlists
     }
 
-    
+    pub fn search_artist(&mut self, search_query: &str, limit:u32) -> Result<(), ClientError> {
+        let seartch_spotify = self.spotify.search(&search_query, SearchType::Artist, None, None, Some(limit), None); 
+       
+        match seartch_spotify {
+            Ok(SearchResult::Artists(artist_page))=>{
+                //println!("this is the artist page {:#?}", artist_page);
+                let mut  result_vec: Vec<artist_results> = Vec::new(); 
+               
+                for artist_page in artist_page.items{    
+                    let result = artist_results {
+                        name: artist_page.name, 
+                        artist_id: artist_page.id,
+                    };
+                    self.results_artist.push(result);
+                }
+                Ok(())
+            }
+            Ok(_) =>{
+                println!("not an artist");
+                Ok(())
+            }
+            Err(e) => {
+                println!("printing error {:?}", e); 
+                Err(e)
+            }
+        }
 
+    }
+
+    pub fn search_album(&mut self, search_query: &str, limit:u32) -> Result<(), ClientError>{
+        let seartch_spotify = self.spotify.search(&search_query, SearchType::Album, None, None, Some(limit), None); 
+       
+        match seartch_spotify {
+            Ok(SearchResult::Albums(album_page))=>{
+                //println!("this is the artist page {:#?}", album_page);                
+                for album_page in album_page.items{   
+
+                    let artist_name = album_page.artists.get(0)
+                        .map(|artist| artist.name.clone())
+                        .unwrap_or_else(|| "Unknown Artist".to_string());
+
+                    let result = album_reslults {
+                        album_name: album_page.name, 
+                        album_maker: artist_name, 
+                        album_id: album_page.id.expect("msg"),
+                    };
+                    self.results_album.push(result);
+                }
+                for rec in &self.results_album {
+                   println!("Album Name: {}", rec.album_name);
+                   println!("Album maker: {}", rec.album_maker);
+                    // println!("Playable ID: {:?}", rec.playable_id); // Use {:?} if `playable_id` implements Debug
+                    println!("------------------------------------");
+                }
+                Ok(())
+            }
+            Ok(_) =>{
+                println!("not an artist");
+                Ok(())
+            }
+            Err(e) => {
+                println!("printing error {:?}", e); 
+                Err(e)
+            }
+        }
+    }
+    
+    pub fn search_tracks(&mut self, search_query: &str, limit:u32) -> Result<(), ClientError> {
+        let seartch_spotify = self.spotify.search(&search_query, SearchType::Track, None, None, Some(limit), None); 
+        match seartch_spotify {
+            Ok(SearchResult::Tracks(tracks_page))=>{
+                //println!("this is the tracks page {:#?}", tracks_page);
+                for tracks_page in tracks_page.items{
+                    let playable_id = PlayableId::Track(tracks_page.id.clone().expect("Track ID is None"));
+                    // Extract the artist's name
+                    let artist_name = tracks_page.artists.get(0)
+                        .map(|artist| artist.name.clone())
+                        .unwrap_or_else(|| "Unknown Artist".to_string());
+                    
+                    let result = tracs_results {
+                        playable_id: playable_id, 
+                        name: tracks_page.name, 
+                        artist: artist_name, 
+                    };
+                    self.results_tracks.push(result);
+                   // Iterate through and print each track result
+
+                    
+                    
+                }
+                for rec in &self.results_tracks {
+                    println!("Track Name: {}", rec.name);
+                    println!("Artist: {}", rec.artist);
+                // println!("Playable ID: {:?}", rec.playable_id); // Use {:?} if `playable_id` implements Debug
+                    println!("------------------------------------");
+                }
+
+                Ok(())
+            },
+            Ok(_) =>{
+                println!("not a track");
+                let result_vec: Vec<tracs_results> = Vec::new();
+                Ok(())
+            },
+            Err(e) => {
+                println!("printing error {:?}", e); 
+                Err(e)
+            }
+        }
+    }
+
+    pub fn play_song(&mut self, song: usize){
+        if let Some(first_playable_id) = self.results_tracks.get(song).and_then(|track|Some(track.playable_id.clone_static())){
+            
+            let playsong = self
+                .spotify
+                .start_uris_playback(vec![first_playable_id], None, None, None);
+
+            match playsong {
+                Ok(data) => println!("{:#?}", data),
+                Err(e) => println!("Error: {:?}", e),
+            }
+        }else {
+            println!("Error: Could not find a valid playable id for the selected track")
+        }
+    }
+    pub fn play_album(&mut self, album: usize){
+        if let Some(playable_album) = self.results_album.get(album).and_then(|album|Some(album.album_id.clone_static())){
+            
+            let playsong = self
+                .spotify
+                .start_context_playback(rspotify::prelude::PlayContextId::Album(playable_album), None, None, None);
+
+            match playsong {
+                Ok(data) => println!("{:#?}", data),
+                Err(e) => println!("Error: {:?}", e),
+            }
+        }else {
+            println!("Error: Could not find a valid playable id for the selected track")
+        }
+    }
+
+    pub fn play_artist(&mut self, artist: usize){
+        if let Some(playable_artist) = self.results_artist.get(artist).and_then(|artist|Some(artist.artist_id.clone_static())){
+            
+            let playsong = self
+                .spotify
+                .start_context_playback(rspotify::prelude::PlayContextId::Artist(playable_artist), None, None, None);
+
+            match playsong {
+                Ok(data) => println!("{:#?}", data),
+                Err(e) => println!("Error: {:?}", e),
+            }
+        }else {
+            println!("Error: Could not find a valid playable id for the selected track")
+        }
+    }
 }
+
